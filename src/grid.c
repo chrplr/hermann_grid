91d56ee4435b1f7a29d449daa171eecca57e18f7
Christophe Pallier <christophe@pallier.org>
2021-05-16 11:59:58 +0200

    wait() renamed delay() to avoid name conflict under MacOSX

--- src/grid.c
+++ src/grid.c
@@ -2,7 +2,7 @@
 
 /* Copyright © Christophe Pallier
 
-   Time-stamp: <2021-05-03 20:09:12 christophe@pallier.org> 
+   Time-stamp: <2021-05-16 11:30:14 christophe@pallier.org> 
 
    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -70,7 +70,7 @@ display ()
 };
 
 inline void
-wait (int delay_in_ms)
+delay (int delay_in_ms)
 {
      SDL_Delay (delay_in_ms);
 };
@@ -88,7 +88,7 @@ main ()
      {
           render_grid (n_cells, cell_width, gap, sdlRenderer);
           display ();
-          wait (10);
+          delay (10);
           process_keys ();
      }
 
